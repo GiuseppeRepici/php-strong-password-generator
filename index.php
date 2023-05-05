db62c86522db23e37bd693609d1a0eaa01ceb40b
GiuseppeRepici <giuseppe.repici95@gmail.com>
2023-05-05 15:18:21 +0200

    un po di stile

--- index.php
+++ index.php
@@ -17,15 +17,19 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
-<body>
-    <form class="d-flex" action="index.php" method="GET">
-            <div class="mb-4">
-                <label for="lunghezza">lunghezza password</label>
-                <input type="text" id="lunghezza" name="lunghezza">
+<body class="bg-primary">
+    <h1 class="text-center">STRONG PASSWORD GENERATOR</h1>
+    <H2 class="text-center">GENERA UNA PASSWORD SICURA</H2>
+<div class="container bg-light">
+    <form action="index.php" method="GET">
+            <div class="mb-4 d-flex justify-content-between">
+                <label class="" for="lunghezza">Lunghezza password:</label>
+                <input class="" type="text" id="lunghezza" name="lunghezza">
             </div>
-            <button type="submit" class="btn btn-primary">Invia</button>
+            <button type="submit" class="mb-5 btn btn-primary">Invia</button>
+            <button type="reset" class="mb-5 btn btn-secondary">Annulla</button>
     </form>
-
+</div>
    
     
 </body>
