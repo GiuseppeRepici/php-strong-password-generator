5d9806933d0ba27264ad54b1e1c959b2af1bf3d8
GiuseppeRepici <giuseppe.repici95@gmail.com>
2023-05-05 14:54:58 +0200

    milestone 3

--- funzione.php
+++ funzione.php
@@ -5,7 +5,9 @@ function generaStringaRandom($lunga) {
     for ($i = 0; $i < $lunga; $i++) {
         $stringaRandom .= $caratteri[rand(0, strlen($caratteri) - 1)];
     }
-    return $stringaRandom;
+    session_start();
+        $_SESSION["password"] = $stringaRandom;
+        header("Location: session.php");
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,12 @@
+<?php session_start();?>
+
 <?php include __DIR__ ."/funzione.php"?>
+<?php 
+    if(isset($_GET["lunghezza"])){
+    $lunghezza = $_GET["lunghezza"];
+    generaStringaRandom($lunghezza); 
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,19 +18,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-    <form action="index.php" method="GET">
+    <form class="d-flex" action="index.php" method="GET">
             <div class="mb-4">
                 <label for="lunghezza">lunghezza password</label>
                 <input type="text" id="lunghezza" name="lunghezza">
             </div>
-            <button class="btn btn-primary">Invia</button>
+            <button type="submit" class="btn btn-primary">Invia</button>
     </form>
 
-    <h2><?php 
-    if(isset($_GET["lunghezza"])){
-    $lunghezza = $_GET["lunghezza"];
-    echo generaStringaRandom($lunghezza); 
-    }
-    ?></h2>
+   
+    
 </body>
 </html>
